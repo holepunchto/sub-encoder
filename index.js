14257741147ad65a40c87359f5e777e0ead3a850
Andrew Osheroff <andrewosh@gmail.com>
2022-10-12 17:30:32 +0200

    Make SubEncoder an actual encoder

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const codecs = require('codecs')
 const b = require('b4a')
 
 const SEP = b.alloc(1)
-const EMPTY = b.alloc(0)
-const MAX = b.from([0xff])
+const SEP_BUMPED = b.from([0x1])
+const GTE_DEFAULT = b.alloc(0)
+const LT_DEFAULT = b.alloc(0)
 
 module.exports = class SubEncoder {
   constructor (prefix, opts) {
@@ -12,49 +13,35 @@ module.exports = class SubEncoder {
       opts = prefix
       prefix = null
     }
-    this.prefix = prefix || null
-    this.keyEncoding = keyEncoding(this.prefix, opts)
+    this.prefix = prefix ? b.concat([prefix, SEP]) : null
+    this.userEncoding = codecs(opts && opts.keyEncoding)
   }
 
-  encodeKey (key) {
-    return this.keyEncoding.encode(key)
+  encode (key) {
+    if (key === LT_DEFAULT) return b.concat([this.prefix.subarray(0, this.prefix.byteLength - 1), SEP_BUMPED])
+    if (key === GTE_DEFAULT) return this.prefix
+    if (this.userEncoding) key = this.userEncoding.encode(key)
+    if (this.prefix) key = b.concat([this.prefix, key])
+    return key
   }
 
-  decodeKey (key) {
-    return this.keyEncoding.decode(key)
+  decode (key) {
+    if (this.prefix) key = key.subarray(this.prefix.byteLength)
+    if (this.userEncoding) key = this.userEncoding.decode(key)
+    return key
   }
 
-  encodeRange (opts = {}) {
-    return encRange(this.keyEncoding, opts, !!this.prefix)
+  range (opts = {}) {
+    opts = { ...opts, keyEncoding: this }
+    if (this.prefix && !opts.gt && !opts.gte) opts.gte = GTE_DEFAULT
+    if (this.prefix && !opts.lt && !opts.lte) opts.lt = LT_DEFAULT
+    return opts
   }
 
   sub (prefix, opts) {
     if (typeof prefix === 'string') {
       prefix = b.from(prefix)
     }
-    return new SubEncoder(this.prefix ? b.concat([this.prefix, SEP, prefix]) : prefix, opts)
+    return new SubEncoder(this.prefix ? b.concat([this.prefix, prefix]) : prefix, opts)
   }
-}
-
-function keyEncoding (prefix, opts) {
-  const userEncoding = codecs(opts && opts.keyEncoding)
-  return {
-    encode (k) {
-      if (userEncoding) k = userEncoding.encode(k)
-      if (prefix) k = b.concat([prefix, SEP, k])
-      return k
-    },
-    decode (k) {
-      if (prefix) k = k.subarray(b.byteLength(prefix) + 1)
-      if (userEncoding) k = userEncoding.decode(k)
-      return k
-    }
-  }
-}
-
-function encRange (keyEncoding, opts, sub) {
-  opts = { ...opts, keyEncoding }
-  if (sub && !opts.gt && !opts.gte) opts.gt = EMPTY
-  if (sub && !opts.lt && !opts.lte) opts.lte = MAX
-  return opts
 }
--- test/all.js
+++ test/all.js
@@ -15,17 +15,17 @@ test('simple sub key encoding', t => {
   const sub2 = sub1.sub('b')
 
   const key = b.alloc(1).fill(1)
-  const k1 = enc.keyEncoding.encode(key)
-  const k2 = sub1.keyEncoding.encode(key)
-  const k3 = sub2.keyEncoding.encode(key)
+  const k1 = enc.encode(key)
+  const k2 = sub1.encode(key)
+  const k3 = sub2.encode(key)
 
   t.alike(k1, key)
   t.alike(k2, b.concat([b.from('a'), SEP, key]))
   t.alike(k3, b.concat([b.from('a'), SEP, b.from('b'), SEP, key]))
 
-  t.alike(key, enc.keyEncoding.decode(k1))
-  t.alike(key, sub1.keyEncoding.decode(k2))
-  t.alike(key, sub2.keyEncoding.decode(k3))
+  t.alike(key, enc.decode(k1))
+  t.alike(key, sub1.decode(k2))
+  t.alike(key, sub2.decode(k3))
 })
 
 test('sub key encoding with hyperbee', async t => {
@@ -35,11 +35,11 @@ test('sub key encoding with hyperbee', async t => {
   const sub1 = enc.sub('hello', { keyEncoding: 'utf-8' })
   const sub2 = enc.sub('world')
 
-  await bee.put('a', b.from('a'), { keyEncoding: sub1.keyEncoding })
-  await bee.put('b', b.from('b'), { keyEncoding: sub2.keyEncoding })
+  await bee.put('a', b.from('a'), { keyEncoding: sub1 })
+  await bee.put('b', b.from('b'), { keyEncoding: sub2 })
 
-  const n1 = await bee.get('a', { keyEncoding: sub1.keyEncoding })
-  const n2 = await bee.get('b', { keyEncoding: sub2.keyEncoding })
+  const n1 = await bee.get('a', { keyEncoding: sub1 })
+  const n2 = await bee.get('b', { keyEncoding: sub2 })
 
   t.is(n1.key, 'a')
   t.alike(n1.value, b.from('a'))
@@ -55,29 +55,29 @@ test('sub range encoding with hyperbee', async t => {
   const subB = enc.sub('sub-b', { keyEncoding: 'utf-8' })
   const subC = enc.sub('sub-c', { keyEncoding: 'utf-8' })
 
-  await bee.put(enc.encodeKey('d1'), 'd2')
-  await bee.put(subA.encodeKey('a1'), 'a1')
-  await bee.put(subB.encodeKey('b1'), 'b1')
-  await bee.put(subB.encodeKey('b2'), 'b2')
-  await bee.put(subB.encodeKey('b3'), 'b3')
-  await bee.put(subC.encodeKey('c1'), 'c1')
+  await bee.put(enc.encode('d1'), 'd2')
+  await bee.put(subA.encode('a1'), 'a1')
+  await bee.put(subB.encode('b1'), 'b1')
+  await bee.put(subB.encode('b2'), 'b2')
+  await bee.put(subB.encode('b3'), 'b3')
+  await bee.put(subC.encode('c1'), 'c1')
 
   {
-    const range = enc.encodeRange({ lt: 'sub' })
+    const range = enc.range({ lt: 'sub' })
     const nodes = await collect(bee.createReadStream(range))
     t.is(nodes.length, 1)
     t.is(nodes[0].key, 'd1')
   }
 
   {
-    const range = subA.encodeRange()
+    const range = subA.range()
     const nodes = await collect(bee.createReadStream(range))
     t.is(nodes.length, 1)
     t.is(nodes[0].key, 'a1')
   }
 
   {
-    const range = subB.encodeRange({ gt: 'b1', lt: 'b3' })
+    const range = subB.range({ gt: 'b1', lt: 'b3' })
     const nodes = await collect(bee.createReadStream(range))
     t.is(nodes.length, 1)
     t.is(nodes[0].key, 'b2')
