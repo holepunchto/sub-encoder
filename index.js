f4a9d5d5d601593deed35ec7eb4e2c9e8ce38eea
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-10-18 18:25:10 +0200

    Support subs of encoded subs

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ module.exports = class SubEncoder {
   }
 
   sub (prefix, encoding) {
-    return new SubEncoder(prefix || EMPTY, compat(encoding), this.prefix)
+    prefix = prefix != null ? this.userEncoding.encode(prefix) : EMPTY
+    return new SubEncoder(prefix, compat(encoding), this.prefix)
   }
 }
 
